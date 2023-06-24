4b82c98674fb0bf7c73eef1b41e7fa9c279ac332
Sam Phillips <samdphillips@gmail.com>
2023-06-24 16:34:45 -0700

    Bump version

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/raco-run.scrbl" ())))
 (define pkg-desc "Run submodules directly")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
 (define raco-commands
